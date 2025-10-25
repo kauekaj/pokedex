9488d132e0d3b218fd6cc1b6b85dbe38fcdb7a97
Kaue Jacyntho <kauekaj@gmail.com>
2025-10-25 10:37:58 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kaue de Assis Jacyntho on 25/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
